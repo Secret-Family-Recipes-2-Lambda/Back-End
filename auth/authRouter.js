3c42ee30cbd4eebf398fba4a9e2b8d688db78030
Mark Hillin <mark.hillin@gmail.com>
2021-03-02 17:49:28 -0600

    Update authRouter.js

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,67 +1,79 @@
 const router = require('express').Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const secrets = require('../config/secrets');
+const secrets = require('../config/secrets.js');
 
-const Users = require('../user/userModel');
-
+const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-    const credentials = req.body;
+  const user = req.body;
 
-    if(Users.isValid(credentials)) {
-      const hash = bcrypt.hashSync(credentials.password, 10)
-      credentials.password = hash;
-      
-      Users.addUser(credentials)
-        .then(user => {
-            const token = generateToken(user)
-          res.status(201).json({
-              newUser: user,
-              token: token,
-              message: 'You have registered successfully'
-            })
-        })
-        .catch(err => res.status(500).json({message: err.message}))
+  if (Users.isValid(user)) {
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
 
-    } else {
-      res.status(400).json({message: 'Please enter a valid username and password'})
-    }
+    Users.add(user)
+      .then((newUser) => {
+        const token = generateToken(newUser);
+        res.status(201).json({
+          newUser: newUser,
+          token: token,
+          message: 'Successful Registeration',
+        });
+      })
+      .catch((err) => res.status(500).json({ message: `Error accessing the database ${err.message}` }));
+  } else {
+    res.status(400).json({ message: 'username and password are required' });
+  }
 });
 
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
+
   if(Users.isValid(req.body)) {
-      Users.findBy({username})
-      .then(([user]) => {
-          if(user && bcrypt.compareSync(password, user.password)) {
-              const token = generateToken(user);
-              res.status(200).json({message: 'You are now logged in', token: token})
-          } else {
-              res.status(401).json({message: 'Please enter the correct username and password'})
-          }
-      })
-      .catch(err => {
-          res.status(500).json({message: err.message})
-      })
+    Users.findBy({username})
+    .then(([ user ]) => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message:  `Welcome, ${user.username}`, token: token})
+      } else {
+        res.status(401).json({ message: 'Please enter the correct username and password' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    })
   } else {
-      res.status(400).json({message: 'please provide a username and password'})
+    res.status(400).json({ message: 'Please provide a username and password' })
+  }
+});
+
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.status(500).json({ message: 'Failed to logout' });
+      } else {
+        res.status(200).json({ message: 'See you later' });
+      }
+    });
   }
 });
 
 function generateToken(user) {
   const payload = {
     subject: user.id,
-    username: user.username
+    username: user.username,
   };
 
   const options = {
-    expiresIn: '1h'
+    expiresIn: '1d',
   };
 
+  const token = jwt.sign(payload, secrets.jwtSecret, options);
 
-  return jwt.sign(payload, secrets.jwtSecret, options);
+  return token;
 }
 
 module.exports = router;
